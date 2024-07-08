e4a78b22b05eac331bb8abebe54f80139a28389a
Ctmax-ui <debjeetbiswas121@gmail.com>
2024-07-08 10:02:53 +0530

    fixed the bug

--- src/components/main/home/Homepage.jsx
+++ src/components/main/home/Homepage.jsx
@@ -6,42 +6,45 @@ import MediaCard from "../../reusableComponents/mediaCard/MediaCard";
 
 const Homepage = () => {
   const [mainCatagory, setMainCatagory] = useState('movie');
-  const [subCatagory, setSubCatagory] = useState();
-  const [currentUrl, setCurrentUrl] = useState()
-
-  const url = (mainCatagory, subCatagory) => {
-    console.log(mainCatagory, subCatagory);
-    if (mainCatagory === "movie") {
-      return `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${subCatagory}`;
-    } else {
-      return `https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${subCatagory}`;
-    }
-    
-  };
-  
-  const { fetchedData } = useMediaFetcher(url(mainCatagory, subCatagory)); 
-  
-  console.log(url(mainCatagory, subCatagory), fetchedData);
-
+  const [subCatagory, setSubCatagory] = useState(null);
+  const [fetchedData, setFetchedData] = useState(null);
 
   const handleMainCatagory = (data) => {
     setMainCatagory(data.id);
+    setSubCatagory(null);
   };
 
   const handleSubCatagory = (data) => {
     setSubCatagory(data.id);
   };
 
+  const url = (mainCatagory, subCatagory) => {
+    if (!subCatagory) return null;
+
+    return mainCatagory === "movie"
+      ? `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${subCatagory}`
+      : `https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${subCatagory}`;
+  };
+
+  const fetchUrl = url(mainCatagory, subCatagory);
+  const { fetchedData: mainResult } = useMediaFetcher(fetchUrl);
+
+  useEffect(() => {
+    if (mainResult && mainResult.total_results >= 0) {
+      setFetchedData(mainResult);
+    }
+  }, [mainResult]);
 
   return (
     <>
       <SelectMainCatagory onChange={handleMainCatagory} />
+      
       <SelectCatagoryMenu
         onChange={handleSubCatagory}
         CatagoryType={mainCatagory}
       />
 
-      <div className="my-5 w-full mx-auto flex flex-wrap gap-3 justify-center">
+      <div className="my-5 w-[98%] mx-auto flex flex-wrap gap-3 justify-center">
         {fetchedData &&
           fetchedData.results.map((result, key) => (
             <MediaCard
